1ca162439efc18f61fe11e89745a76318981afda
rangulvers <msohns@sap.com>
2021-12-08 15:12:36 +0100

    cleanup

--- Cloud_ALM/cf_python/projects/app.py
+++ Cloud_ALM/cf_python/projects/app.py
@@ -49,6 +49,6 @@ def getTasks():
 
 if __name__ == '__main__':
     if cf_port is None:
-        app.run(host='0.0.0.0', port=5000, debug=True)
+        app.run(host='0.0.0.0', port=5000, debug=False)
     else:
-        app.run(host='0.0.0.0', port=int(cf_port), debug=True)
+        app.run(host='0.0.0.0', port=int(cf_port), debug=False)
